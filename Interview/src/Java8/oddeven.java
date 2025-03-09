5b889c031fc8a4ac36536b2ffcea7aa545996d6a
mahesh8497 <maheshmtakale97@gmail.com>
2025-03-09 23:28:37 +0530

    Add

---
+++ Interview/src/Java8/oddeven.java
@@ -0,0 +1,15 @@
+package Java8;
+
+public class oddeven {
+public static void main(String[] args) {
+	int arr[]= {2,3,4,5,6,7,8};
+	for(int a: arr) {
+		if((a & 1)==0) {
+			System.out.println(a+" is even");
+		}
+		else {
+			System.out.println(a+" is odd");
+		}
+	}
+}
+}
