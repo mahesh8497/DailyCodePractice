76b1b1a89e00ea9a88d51892b7958547888feab5
mahesh8497 <maheshmtakale97@gmail.com>
2025-03-18 23:14:55 +0530

    add

--- Interview/src/Java8/oddeven.java
+++ Interview/src/Java8/oddeven.java
@@ -4,7 +4,7 @@ public class oddeven {
 public static void main(String[] args) {
 	int arr[]= {2,3,4,5,6,7,8};
 	for(int a: arr) {
-		if((a & 1)==0) {
+		if((a & 1)==0) {  // this is without using % operator (tricky Question) 
 			System.out.println(a+" is even");
 		}
 		else {
