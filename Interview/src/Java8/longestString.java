e5f440776d41fe36549dfe6e0c6087ddd4c460c6
mahesh8497 <maheshmtakale97@gmail.com>
2025-05-01 23:48:12 +0530

    add

--- Interview/src/Java8/longestString.java
+++ Interview/src/Java8/longestString.java
@@ -10,18 +10,24 @@ import java.util.stream.Collectors;
 public class longestString {
 	public static void main(String[] args) {
 		String s="ndijn abdibcs  njadbkja da  aid aid isdb isbisbfisbf";
-		String longestWord = Arrays.stream(s.split(" ")).max(Comparator.comparingInt(String::length)).orElse("");
+		
+		//Longest word:
+		String longestWord = Arrays.stream(s.split(" "))
+				.max(Comparator.comparingInt(String::length)).orElse("");
 		int length = longestWord.toCharArray().length;
 		System.out.println(length);
 		System.out.println("Longest word:  "+ longestWord);
+
+		//count each characte
+		Map<String, Long>map2=Arrays.stream(longestWord.split("")).map(String::toLowerCase).collect(Collectors.groupingBy(e->e,LinkedHashMap::new,Collectors.counting()));
+		System.out.println("count each characte: "+map2);
 		
 		//remove duplicate
 		String collect = Arrays.stream(longestWord.split("" )).filter(q->!q.isEmpty()).distinct().collect(Collectors.joining(" "));
 		System.out.println("remove duplicate: " +collect);
 		
 		
-		Map<String, Long>map2=Arrays.stream(longestWord.split("")).map(String::toLowerCase).collect(Collectors.groupingBy(e->e,LinkedHashMap::new,Collectors.counting()));
-		System.out.println("count each characte: "+map2);
+		
 	}
 
 }
