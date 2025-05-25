4176c93783198a42c12d6deed133f8155fd77eb4
mahesh8497 <maheshmtakale97@gmail.com>
2025-05-25 22:40:58 +0530

    add

---
+++ Interview/src/Java8/startWith_1.java
@@ -0,0 +1,16 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class startWith_1 {
+	public static void main(String[] args) {
+		List<Integer> li = Arrays.asList(10, 1, 12, 1, 3, 6, 12);
+		 li.stream()
+        .map(s -> s + "") // Convert integer to String
+        .filter(s -> s.startsWith("1"))
+        .forEach(System.out::println);
+	}
+
+}
