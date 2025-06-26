4ecd379d18dc3fe9bf96b88a5284eb2c2e76ae2d
mahesh8497 <maheshmtakale97@gmail.com>
2025-06-26 09:21:41 +0530

    add

---
+++ Interview/src/Java8/EagerSingleton.java
@@ -0,0 +1,74 @@
+package Java8;
+
+public class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {} // private constructor
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
+
+//public class LazySingleton {
+//    private static LazySingleton instance;
+//
+//    private LazySingleton() {}
+//
+//    public static LazySingleton getInstance() {
+//        if (instance == null) {
+//            instance = new LazySingleton(); // not thread-safe
+//        }
+//        return instance;
+//    }
+//}
+
+
+
+//public class ThreadSafeSingleton {
+//    private static ThreadSafeSingleton instance;
+//
+//    private ThreadSafeSingleton() {}
+//
+//    public static synchronized ThreadSafeSingleton getInstance() {
+//        if (instance == null) {
+//            instance = new ThreadSafeSingleton();
+//        }
+//        return instance;
+//    }
+//}
+
+
+
+
+//public class DoubleCheckedSingleton {
+//    private static volatile DoubleCheckedSingleton instance;
+//
+//    private DoubleCheckedSingleton() {}
+//
+//    public static DoubleCheckedSingleton getInstance() {
+//        if (instance == null) {
+//            synchronized (DoubleCheckedSingleton.class) {
+//                if (instance == null) {
+//                    instance = new DoubleCheckedSingleton();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+//}
+
+
+
+
+//public enum EnumSingleton {
+//    INSTANCE;
+//
+//    public void someMethod() {
+//        System.out.println("Doing something...");
+//    }
+//}
+
+
+
+
